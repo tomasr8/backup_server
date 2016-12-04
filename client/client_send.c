acb77600a4d953f1c0d986587751e6c439ad4de8
zedpowa <tomas.roun@seznam.cz>
2016-12-04 23:20:08 +0100

    reworked client_send to use struct

--- client/client_send.c
+++ client/client_send.c
@@ -1,51 +1,100 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <stdint.h>
 #include "err.h"
 
+#define GET 100
+#define SET 200
+#define UNKNOWN -1
 
-void readAndSend(int socket) {
-  //int bytesRead;
-  const int size = 255 + 7 + 1;
-  char buffer[size];
+typedef struct {
+    int code;
+    int len;
+    char data[255];
+} request;
 
-  while(fgets(buffer, size, stdin) != NULL) {
-    char command[4] = {0};
-    int resource;
-    char data[256] = {0};
-    //fprintf(stderr, "Inputed: <%s>\n", buff);
+void readStdin();
+int commandToNumber(char *command);
+void sendRequest(int socket, request *req);
+void parseRequest(char *line, request *req);
 
-    if (buffer[strlen(buffer)-1] != '\n') {
-      char ch;
-      while (((ch = getchar()) != '\n') && (ch != EOF)) {}
+int main() {
+    readStdin();
+
+    return 0;
+}
+
+int commandToNumber(char *command) {
+    if(strcmp("get", command) == 0) {
+        return GET;
+    } else if(strcmp("set", command) == 0) {
+        return SET;
+    } else {
+        fprintf(stderr, "Wrong command\n");
+        return UNKNOWN;
     }
+}
 
-/*    for(int i = 0; i < 3; i++) {
-      command[i] = buffer[i];
-    }
-    command[4] = '\0';
+void sendRequest(int socket, request *req) {
 
-    if(buffer[4] != ' ') {
-      fprintf(stderr, "invalid command");
-      continue;
-    }
+}
 
-    for(int i = 0; i < 3; i++) {
-      command[i] = buffer[i];
-    }
-    command[4] = '\0';*/
-
+void parseRequest(char *buffer, request *req) {
+    char command[4];
+    char data[257];
+    int resource = -1;
     int numRead;
-    numRead = sscanf(buffer, "%3s %d %255s", command, resource, data);
 
-    if (numRead < 2) {
-      fprintf(stderr, "invalid command");
+    numRead = sscanf(buffer, "%3s%*[ ]%d%256[^\n]s", command, &resource, data);
+
+    if(resource < 0 || resource > 33) {
+        fprintf(stderr, "Invalid resource: %d\n", resource);
+        req->code = UNKNOWN;
+        return;
     }
 
-    
+    int code = commandToNumber(command);
 
-    //numread = sscanf(buff, "%d %d\n", &a, &b);
-    //fprintf(stderr, "sscanf read %d numbers\n", numread);
-  }
+    if(code == UNKNOWN) {
+        fprintf(stderr, "Command number < 0\n");
+        req->code = UNKNOWN;
+        return;
+    }
 
+    if(!(code == 100 && numRead == 2) && !(code == 200 && numRead == 3 && data[0] == ' ')) {
+        fprintf(stderr, "Invalid command: %d, %d\n", code, numRead);
+        req->code = UNKNOWN;
+        return;
+    }
+
+    req->code = code + resource;
+    req->len = strlen(data) - 1;
+    strncpy(req->data, data+1, 255);
+}
+
+void readStdin() {
+    const int size = 255 + 7 + 1;
+    char buffer[size];
+
+    while(fgets(buffer, size, stdin) != NULL) {
+    //   char command[4] = {0};
+    //   int resource = -1;
+    //   char data[257] = {0};
+
+      if (buffer[strlen(buffer)-1] != '\n') {
+        char ch;
+        while (((ch = getchar()) != '\n') && (ch != EOF)) {}
+      }
+
+      request req;
+
+      parseRequest(buffer, &req);
+
+      if(req.code == UNKNOWN) {
+          continue;
+      }
+
+      printf("to Send> %d %d <%s>\n", req.code, req.len, req.data);
+    }
 }
